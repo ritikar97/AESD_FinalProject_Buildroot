ae1f519508dcae1f000e1451bcf54c66773a1c49
ritikar97 <rira3427@colorado.edu>
2023-04-23 16:34:49 -0600

    Working with temperature sensor

--- base_external/package/bme280-driver/bme280-driver.mk
+++ base_external/package/bme280-driver/bme280-driver.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-BME280_DRIVER_VERSION = '8ae994dfe336b95d77ab53a88c90966d9862c351'
+BME280_DRIVER_VERSION = '439b75f438b530fa8e3efcdc5f4f9b57a6e80dc7'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,13 +14,18 @@ BME280_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/final-project-ritikar97.git'
 BME280_DRIVER_SITE_METHOD = git
 BME280_DRIVER_GIT_SUBMODULES = YES
 
-
 BME280_DRIVER_MODULE_SUBDIRS = bme280-driver
+
+define BME280_DRIVER_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/measure all
+endef
+
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define BME280_DRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/measure/bme280_measure $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/bme280-driver/bme280_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/bme280-driver/bme280_unload $(TARGET_DIR)/usr/bin
+    $(INSTALL) -m 0755 $(@D)/bme280-driver/bme280_unload $(TARGET_DIR)/usr/bin
 endef
 
 
---
+++ base_external/rootfs_overlay/etc/init.d/S98bmemodules.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+case "$1" in
+    start)
+        echo "Starting bme280 driver"
+        modprobe i2c-dev
+        modprobe i2c-bcm2835
+        /usr/bin/bme280_load  
+        ;;
+    stop)
+        echo "Unloading bme280 driver"
+        /usr/bin/bme280_unload 
+        rmmod i2c-bcm2835
+        rmmod i2c-dev
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+    exit 1
+esac
+
+exit 0
