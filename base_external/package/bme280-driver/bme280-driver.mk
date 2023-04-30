ede66afad3a7cdd0d7cc28a3ab3d4ce4e510d9a3
ritikar97 <rira3427@colorado.edu>
2023-04-30 13:28:29 -0600

    Pushing with MQTT

--- base_external/package/bme280-driver/bme280-driver.mk
+++ base_external/package/bme280-driver/bme280-driver.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-BME280_DRIVER_VERSION = '439b75f438b530fa8e3efcdc5f4f9b57a6e80dc7'
+BME280_DRIVER_VERSION = 'ca9fb5b18c239591d60616d5c923db13ea1c56d9'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -18,6 +18,7 @@ BME280_DRIVER_MODULE_SUBDIRS = bme280-driver
 
 define BME280_DRIVER_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/measure all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/mqtt_sub all
 endef
 
 
@@ -26,6 +27,7 @@ define BME280_DRIVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/measure/bme280_measure $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/bme280-driver/bme280_load $(TARGET_DIR)/usr/bin
     $(INSTALL) -m 0755 $(@D)/bme280-driver/bme280_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/mqtt_sub/mqtt_sub_rpi $(TARGET_DIR)/usr/bin
 endef
 
 
