2c2ccb6ff79b3441f0954bab9223f173e5940950
ritikar97 <rira3427@colorado.edu>
2023-04-30 23:43:26 -0600

    WOrking project

--- base_external/package/bme280-driver/bme280-driver.mk
+++ base_external/package/bme280-driver/bme280-driver.mk
@@ -5,11 +5,7 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-BME280_DRIVER_VERSION = 'ca9fb5b18c239591d60616d5c923db13ea1c56d9'
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
+BME280_DRIVER_VERSION = '2ca85071074d8010e42207f0a143dc687cd06002'
 BME280_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/final-project-ritikar97.git'
 BME280_DRIVER_SITE_METHOD = git
 BME280_DRIVER_GIT_SUBMODULES = YES
@@ -18,16 +14,13 @@ BME280_DRIVER_MODULE_SUBDIRS = bme280-driver
 
 define BME280_DRIVER_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/measure all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/mqtt_sub all
 endef
 
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define BME280_DRIVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/measure/bme280_measure $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/bme280-driver/bme280_load $(TARGET_DIR)/usr/bin
     $(INSTALL) -m 0755 $(@D)/bme280-driver/bme280_unload $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/mqtt_sub/mqtt_sub_rpi $(TARGET_DIR)/usr/bin
 endef
 
 
--- base_external/rootfs_overlay/etc/init.d/S99mosquitto.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/sbin/mosquitto -c /etc/mosquitto.conf
